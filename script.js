8cbe5496102371266e9973594cc18c4edf7c5a67
Sebastian Patron <sebastian.patrones@gmail.com>
2017-01-24 13:13:38 -0700

    redid asteroid movement

--- script.js
+++ script.js
@@ -10,43 +10,46 @@ const ctx = canvas.getContext('2d');
 // const points = [40, 40, 20, 20, 10, 30, 60, 60, 40, 40];
 let points = [-40, -20, -20, -40, 0, -20, 20, -40, 40, -20, 30, 0, 40, 20, 10, 40, -40, 20, -40, -20];
 
-function draw() {
-    //draw the state of the world
-    ctx.clearRect(0,0, width, height);
-    ctx.save();
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    for (let i = 0; i < points.length; i += 2 ) {
-      if (i == 0) {
-        ctx.moveTo(points[i] + 50, points[i+1]+ 50);
-        console.log("move to line");
-      } else {
-        ctx.lineTo(points[i]  + 50, points[i+1] + 50);
-      }
-    }
+let asteroid = {
+  position: {
+    x: 400,
+    y: 400
+  },
+  
+  movement: {
+    x: 3,
+    y: 5
+  },
 
-    // let progress = timestamp - lastrender;
-    i = 0;
-
-    ctx.stroke();
-    ctx.restore();
-    // rotate(p, 0.01);
-    //console.log("draw executed!");
-    //lastrender = timestamp;
-
-    //window.requestAnimationFrame(draw);
-
-    //ctx.fillRect(state.x - 5,  state.y - 5, 10, 10);
+  rotation: 0
 }
 
+function draw() {
+  ctx.save();
+  ctx.fillRect(asteroid.position.x, asteroid.position.y, 50, 50);
+  ctx.beginPath();
+  for (let i = 0; i < points.length; i += 2 ) {
+    if (i == 0) {
+      ctx.moveTo(points[i] + 50, points[i+1]+ 50);
+      console.log("move to line");
+    } else {
+      ctx.lineTo(points[i]  + 50, points[i+1] + 50);
+    }
+  }
+  ctx.stroke();
+}
 
 function move() {
-
+  asteroid.position.x += asteroid.movement.x;
+  asteroid.position.y += asteroid.movement.y;
+  if (asteroid.position.x > canvas.width) asteroid.position.x -= canvas.width;
+  if (asteroid.position.y > canvas.height) asteroid.position.y -= canvas.height;
   for (let i = 0; i < points.length; i += 2) {
-  points[i] = points[i] + 2;
-  points[i + 1] = points[i + 1] + 2;
+      points[i] += asteroid.movement.x;
+      points[i+1] += asteroid.movement.y;
   }
 }
+
 
 function loop(timestamp) {
     let progress = timestamp - lastrender;
@@ -55,6 +58,9 @@ function loop(timestamp) {
 
     //update(progress);
     ctx.clearRect(0,0, width, height);
+    asteroid.rotation += (progress/16) *5;
+    ctx.rotate((Math.PI/180) * asteroid.rotation);
+    console.log(asteroid.rotation);
     draw();
     move();
 
@@ -80,5 +86,49 @@ function loop(timestamp) {
 //   for (let i = 0; i < points.length; i += 2) {
 //     points[i] = p * .3 * Math.cos((rotation - 90) * (Math.PI / 180));
 //     points[i + 1] = p * .3 * Math.sin((rotation - 90) * (Math.PI / 180));
+//   }
+// }
+
+
+
+
+//asteroid implementation 1 code below
+
+
+// function draw() {
+//     //draw the state of the world
+//     ctx.clearRect(0,0, width, height);
+//     ctx.save();
+//     ctx.lineWidth = 2;
+//     ctx.beginPath();
+//     for (let i = 0; i < points.length; i += 2 ) {
+//       if (i == 0) {
+//         ctx.moveTo(points[i] + 50, points[i+1]+ 50);
+//         console.log("move to line");
+//       } else {
+//         ctx.lineTo(points[i]  + 50, points[i+1] + 50);
+//       }
+//     }
+
+//     // let progress = timestamp - lastrender;
+//     i = 0;
+
+//     ctx.stroke();
+//     ctx.restore();
+//     // rotate(p, 0.01);
+//     //console.log("draw executed!");
+//     //lastrender = timestamp;
+
+//     //window.requestAnimationFrame(draw);
+
+//     //ctx.fillRect(state.x - 5,  state.y - 5, 10, 10);
+// }
+
+
+// function move() {
+
+//   for (let i = 0; i < points.length; i += 2) {
+//   points[i] = points[i] + 2;
+//   points[i + 1] = points[i + 1] + 2;
 //   }
 // }
